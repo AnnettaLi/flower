f309f3b7eca50889a25c46a55d6bf132f841588b
kehui.li-i <kehui.li-i@qorosauto.com>
2020-05-09 17:46:38 +0800

    详情页

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,11 @@ export default new Vuex.Store({
         src: "http://img.weiye.me/zcimgdir/album/file_58c274afbfcb9.jpg",
         title: "触摸----薰衣草礼盒：进口薰衣草永生花",
         price: "300.00",
-        sales: "0"
+        sales: "0",
+        imgList:[
+          {url:"http://img.weiye.me/zcimgdir/image/20170310174039_85802.jpg"},
+          {url:"http://img.weiye.me/zcimgdir/image/20170310174055_41184.jpg"},
+        ]
       },
       {
         id: "2",
@@ -19,7 +23,12 @@ export default new Vuex.Store({
           "http://img.weiye.me/zcimgdir/thumb/t_148913862958c273c5af543.jpg",
         title: "十二星座经典七彩·摩羯座----彩虹永生玫瑰:厄瓜多尔进口巨型玫瑰",
         price: "288.00",
-        sales: "3"
+        sales: "3",
+        imgList:[
+          {url:"http://img.weiye.me/zcimgdir/image/20170310173724_58845.jpg"},
+          {url:"http://img.weiye.me/zcimgdir/image/20170310173743_13436.jpg"},
+          {url:"http://img.weiye.me/zcimgdir/image/20170310173753_63619.jpg"},
+        ]
       },
       {
         id: "3",
@@ -28,7 +37,11 @@ export default new Vuex.Store({
         title:
           "甜蜜物语----黛安娜粉玫瑰33枝，粉色香水百合3枝，搭配适量粉色勿忘我",
         price: "358.00",
-        sales: "1"
+        sales: "1",
+        imgList:[
+          {url:"http://img.weiye.me/zcimgdir/image/20170310173548_51631.jpg"},
+          {url:"http://img.weiye.me/zcimgdir/image/20170310173558_25802.jpg"},
+        ]
       },
       {
         id: "4",
@@ -36,7 +49,10 @@ export default new Vuex.Store({
           "http://img.weiye.me/zcimgdir/thumb/t_148913841658c272f01c36b.jpg",
         title: "芍药",
         price: "100.00",
-        sales: "2"
+        sales: "2",
+        imgList:[
+          {url:"http://img.weiye.me/zcimgdir/image/20170310173347_62019.jpeg"},
+        ]
       },
       {
         id: "5",
@@ -44,21 +60,35 @@ export default new Vuex.Store({
           "http://img.weiye.me/zcimgdir/thumb/t_148913816858c271f8ca9f1.jpg",
         title: "绣球",
         price: "100.00",
-        sales: "0"
+        sales: "0",
+        imgList:[
+          {url:"http://img.weiye.me/zcimgdir/image/20170310172438_92699.jpeg"},
+        ]
       },
       {
         id: "6",
         src: "http://img.weiye.me/zcimgdir/album/file_58c26f8a85750.jpg",
         title: "触摸----薰衣草礼盒：进口薰衣草永生花",
         price: "378.00",
-        sales: "0"
+        sales: "0",
+        imgList:[
+          {url:"http://img.weiye.me/zcimgdir/image/20170310171942_61956.jpg"},
+          {url:"http://img.weiye.me/zcimgdir/image/20170310171951_65108.jpg"},
+          {url:"http://img.weiye.me/zcimgdir/image/20170310172001_14188.jpg"},
+        ]
       },
       {
         id: "7",
         src: "http://img.weiye.me/zcimgdir/album/file_58c26e655cd90.jpg",
         title: "佳期如梦----永生花盒：进口永生玫瑰3枝,永生康乃馨3枝",
         price: "336.00",
-        sales: "0"
+        sales: "0",
+        imgList:[
+          {url:"http://img.weiye.me/zcimgdir/image/20170310171620_62375.jpg"},
+          {url:"http://img.weiye.me/zcimgdir/image/20170310171628_36538.jpg"},
+          {url:"http://img.weiye.me/zcimgdir/image/20170310171638_56279.jpg"},
+          {url:"http://img.weiye.me/zcimgdir/image/20170310171649_89015.jpg"},
+        ]
       }
     ]
   },
--- src/views/flower/flowerDetail.vue
+++ src/views/flower/flowerDetail.vue
@@ -2,14 +2,38 @@
   <div class="flower-detail">
     <div class="detail-full" v-for="item in detailList" :key="item.id">
       <img :src="item.src" alt class="detail-banner" />
-      <div class="goods-header p-10">
+      <div class="goods-header p-l-r-10">
         <h2>{{item.title}}</h2>
-        <span>{{item.price}}</span>
+        <span class="iconfont iconqian1 p-t-b-10 bold">{{item.price}}</span>
         <h4>配送费：包邮</h4>
-        <div class="goods-info-other">
-          
+        <div class="goods-info-other p-t-b-5">
+          <h4 class="w-20">取货方式</h4>
+          <i class="w-65 m-right-5 text-right el-icon-circle-check"></i>
+          <h4 class="w-10">快递</h4>
         </div>
       </div>
+      <div class="detail-line">
+        <div class="line"></div>
+        <h2 class="el-icon-picture m-left-5 m-right-5"></h2>
+        <h4 class="m-right-5">详情</h4>
+        <div class="line"></div>
+      </div>
+      <h3 class="m-left-10 m-right-10 p-t-b-10 product-detail">产品详情</h3>
+
+      <div class="imgList p-10">
+        <div class="imgbox" v-for="i in item.imgList" :key="i.url">
+          <img :src="i.url" alt />
+        </div>
+      </div>
+    </div>
+    <div class="goods-action">
+      <van-goods-action>
+        <van-goods-action-icon icon="chat-o" text="客服" />
+        <van-goods-action-icon icon="cart-o" text="购物车" />
+         <van-goods-action-icon icon="star" text="已收藏" color="#ff5000" />
+        <van-goods-action-button type="warning" text="加入购物车" />
+        <van-goods-action-button type="danger" text="立即购买" />
+      </van-goods-action>
     </div>
   </div>
 </template>
@@ -17,6 +41,13 @@
 <script>
 import Vue from "vue";
 import { mapState } from "vuex";
+import { GoodsAction, GoodsActionIcon, GoodsActionButton,ActionSheet,Stepper} from "vant";
+Vue.use(GoodsAction);
+Vue.use(GoodsActionButton);
+Vue.use(GoodsActionIcon);
+Vue.use(ActionSheet);
+Vue.use(Stepper);
+
 
 import { Input, Button } from "element-ui";
 Vue.use(Input);
@@ -25,7 +56,8 @@ Vue.use(Button);
 export default {
   data() {
     return {
-      detailList: []
+      detailList: [],
+      checked:true
     };
   },
   methods: {},
@@ -44,8 +76,11 @@ export default {
 </script>
 
 <style lang="less">
+@import url(//at.alicdn.com/t/font_1806067_p0w4b1lr16.css);
 .flower-detail {
   width: 100%;
+  height: 100%;
+  
   h2 {
     font-size: 0.16rem;
   }
@@ -56,10 +91,67 @@ export default {
     font-size: 0.12rem;
   }
   .detail-full {
+    width: 100%;
+    height: 100%;
     .detail-banner {
       width: 100%;
       height: 3.75rem;
     }
+    .goods-header {
+      width: 95%;
+      // height: 1.35rem;
+      display: flex;
+      flex-direction: column;
+      h4 {
+        color: #666666;
+      }
+      span {
+    font-size: 0.2rem;
+    color: #ff7100;
+  }
+      .goods-info-other {
+        display: flex;
+        color: #666666;
+        i {
+          font-size: 0.15rem;
+          color: #ff7100;
+        }
+      }
+    }
+    .detail-line {
+      width: 100%;
+      height: 0.38rem;
+      background: rgb(244, 244, 244);
+      color: #cccccc;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .line {
+        width: 0.46rem;
+        height: 0.01rem;
+        background-color: #cccccc;
+      }
+    }
+    .product-detail {
+      border-bottom: 1px solid rgb(244, 244, 244);
+    }
+    .imgList {
+      width: 95%;
+      display: flex;
+      flex-direction: column;
+      .imgbox {
+        width: 100%;
+        img {
+          width: 100%;
+          height: auto;
+        }
+      }
+    }
+  }
+  .goods-action{
+    width: 100%;
+    font-size: 0.12rem;
   }
 }
 </style>
